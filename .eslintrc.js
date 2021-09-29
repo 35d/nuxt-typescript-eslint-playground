7c8cb158a0e7007fbb6edec75261e00aa66f0a6d
35d <xxx35d@gmail.com>
2021-09-30 00:26:29 +0900

    🎉 initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'vue/no-unused-properties': [
+      'error',
+      {
+        groups: ['props', 'setup'],
+      },
+    ],
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const a = 1 // 使用していない変数
+    return {
+      a,
+    }
+  },
+})
+</script>
